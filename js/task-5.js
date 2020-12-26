66edc3ef743025c1f15523b3b67b4d0d5b139c7f
Anton-Vovk <mr.vovk.anton@gmail.com>
2020-12-26 17:07:32 +0200

    js-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,10 +1,55 @@
 
+class Car {
+  static getSpecs(car) {
+    console.log(
+      `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`,
+    );
+  }
 
+  constructor({ speed = 0, price, maxSpeed, isOn = false, distance }) {
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
 
+  get price() {
+    return this._price;
+  }
 
+  set price(newPrice) {
+    this._price = newPrice;
+  }
 
+  turnOn() {
+    this.isOn = true;
+    return this.isOn;
+  }
 
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
 
+  accelerate(value) {
+    if (value < this.maxSpeed) {
+      this.speed += value;
+    }
+  }
+
+  decelerate(value) {
+    if (value > 0) {
+      this.speed -= value;
+    }
+  }
+
+  drive(hours) {
+    if (this.turnOn) {
+      this.distance = this.speed * hours;
+    }
+  }
+}
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
 
